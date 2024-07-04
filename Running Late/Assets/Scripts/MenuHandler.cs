8d62a4a6fb2e17c23a170705e0ae5d3ff4a698ad
Emanuel Gonzalez <emanuelg2000@gmail.com>
2024-07-04 13:53:06 -0500

    Main Menu up and going
    Basic main menu with function play and quit button. Will work on having a workable option button and scene later on.

---
+++ Running Late/Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour
+{
+    //When the player presses the Play button, it will bring them to the "Game" Scene
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+
+    }
+
+    //When the player presses the Exit button, it will exit out the game
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
