4198fb2a628244acb4c1f9e9cc6da41047a0e8b4
Emanuel Gonzalez <emanuelg2000@gmail.com>
2024-08-05 16:26:46 -0500

    Options menu updated

--- Running Late/Assets/Scripts/SettingsHandler.cs
+++ Running Late/Assets/Scripts/SettingsHandler.cs
@@ -15,13 +15,21 @@ public class SettingsHandler : MonoBehaviour
 
         List<string> options = new List<string>();
 
+        int currResolutions = 0;
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + "x" + resolutions[i].height + "@" + resolutions[i].refreshRateRatio + "hz";
             options.Add(option);
+
+            if ((resolutions[i].width == Screen.currentResolution.width) && (resolutions[i].height == Screen.currentResolution.height))
+            {
+                currResolutions = i;
+            }
         }
 
         Resolution.AddOptions(options);
+        Resolution.value = currResolutions;
+        Resolution.RefreshShownValue();
     }
     public void Setvolume (float volume)
     {
@@ -36,5 +44,11 @@ public class SettingsHandler : MonoBehaviour
     public void Fullscreen(bool fullscreen)
     {
         Screen.fullScreen = fullscreen;
+    }
+
+    public void setRes(int resIndex)
+    {
+        Resolution resolution = resolutions[resIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
